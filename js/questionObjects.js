9c5428b2234a84c37c56af62de23772a4cb41c98
TKForgeron <trollololollolol.js@gmail.com>
2021-03-05 13:46:48 +0100

    question refactor

---
+++ js/questionObjects.js
@@ -0,0 +1,9 @@
+let mpQuestion1 = new MultipleChoice(
+    1,
+    "question1?",
+    "/images/clientSide.svg",
+    "what is question?",
+    "explanation",
+    "TRUE answer",
+    ["incorrect option", "false again","0"]
+);
