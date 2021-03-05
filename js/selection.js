0cc52ed2cb02692dd1213ad2d066e3af05f0f9cd
DouweGeurtjens <d.geurtjens@students.uu.nl>
2021-03-05 09:21:48 +0100

    selection finished

--- js/selection.js
+++ js/selection.js
@@ -7,6 +7,10 @@ var selectableElements = [
                             [document.getElementsByTagName("footer"),"Footer"]
                         ];
 
+var selectableColors = ["red","green","blue","yellow","white","black"];
+
+var selectableFontSizes = ["8","10","12","14"];
+
 var dropdownBox = document.createElement("select");
 dropdownBox.classList.add("header__menu");                    
 
@@ -23,7 +27,8 @@ applyButton.classList.add("header__button");
 applyButton.appendChild(document.createTextNode("Apply"));
 
 // Call this with selectableElements to add all the html elements to the select drop down menu
-function addToDropDown(selectableCollection) {
+// This is the advanced version because it actually takes html elements as well rather than just tags, so we need to change the functionality so much (compared to addToBasicDropDown()) that we might as well make it a different function.
+function addToAdvancedDropDown(selectableCollection) {
     for (let i = 0; i < selectableCollection.length; i++) {
         // Get the tag plus associated element from the original array
         var collection = selectableCollection[i][0];
@@ -41,13 +46,28 @@ function addToDropDown(selectableCollection) {
         }
     }
 }
+// Takes a list of strings (tags) and a html select elements and adds the given tags as options
+function addToBasicDropDown(selectableCollection, selectBox){
+    for (let i = 0; i < selectableCollection.length; i++) {
+        // Get the tag we want
+        var tag = selectableCollection[i]
+        var option = document.createElement("option");
+        option.text = tag;   
+        selectBox.add(option);        
+    }
+}
 // When we click apply we do the things that are requested
-// Currently just disables the element as a placeholder
+// We add px to the end for the fontsize settings, rather than doing it in the original list because this is more flexible.
 applyButton.addEventListener("click", function(){
-    dropdownBoxObjects[dropdownBox.selectedIndex].style.display = "none";
+    dropdownBoxObjects[dropdownBox.selectedIndex].style.backgroundColor = selectableColors[backgroundColorBox.selectedIndex];
+    dropdownBoxObjects[dropdownBox.selectedIndex].style.fontSize = selectableFontSizes[fontSizeBox.selectedIndex] + "px";
 });
 
-addToDropDown(selectableElements);
+addToAdvancedDropDown(selectableElements);
+addToBasicDropDown(selectableColors,backgroundColorBox);
+addToBasicDropDown(selectableFontSizes,fontSizeBox);
 header = document.getElementsByTagName("header")[0];
 header.appendChild(dropdownBox);
+header.appendChild(backgroundColorBox);
+header.appendChild(fontSizeBox);
 header.appendChild(applyButton);
