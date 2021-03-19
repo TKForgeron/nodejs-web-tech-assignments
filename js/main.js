fe5e83206aedaee21bec4f9426781738686ae1e7
Maarten Henzen <m.henzen@students.uu.nl>
2021-03-19 12:32:51 +0100

    Main.js updated (assignment 3)

--- js/main.js
+++ js/main.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World.');
-}).listen(PORTNUMBER,22);
+}).listen(8015, 'localhost');
