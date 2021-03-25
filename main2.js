c8c5c7f5c5f8c747973d888d06ba715f6c35e45a
DouweGeurtjens <d.geurtjens@students.uu.nl>
2021-03-25 12:57:10 +0100

    PUG routing

---
+++ main2.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use('/assignment3', express.static(__dirname + '/assets'));
+
+app.get('/helloThere', function(request, response) {
+	response.send('Hello there, from me!')
+});
+
+app.listen(8015, function(){
+	console.log('Listening at Port 8015');
+})
