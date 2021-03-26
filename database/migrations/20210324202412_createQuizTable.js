f3b958581455748f9e78616c2345302f55bcbe0e
t.k.smit <t.k.smit@students.uu.nl>
2021-03-26 08:51:20 +0100

    rm .vscode

--- database/migrations/20210324202412_createQuizTable.js
+++ database/migrations/20210324202412_createQuizTable.js
@@ -1,32 +1,36 @@
-
-exports.up = function(knex) {
-    return knex.schema.createTable('quizzes', table => {
-        table.increments('quizId');
-        table.text('title', 128).notNullable();
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('quizzes', table => {
+      table.increments('quizId');
+      table.text('title', 128).notNullable();
     })
     .createTable('questions', table => {
-        table.increments('questionId');
-        table.text('title', 128).notNullable();
-        table.text('image', 128).notNullable();
-        table.text('explanation', 128).notNullable();
-        table.text('answer', 128).notNullable();
-        table.text('otherOptions'); // is NULL for open questions
-        // foreign key info to 'quizzes' table
-        table.integer('quizId_fk')
-            .unsigned()
-            .references('quizId')
-            .inTable('quizzes')
-            .onDelete('CASCADE')
-            .onUpdate('CASCADE')
+      table.increments('questionId');
+      table.text('title', 128).notNullable();
+      table.text('image', 128).notNullable();
+      table.text('explanation', 128).notNullable();
+      table.text('answer', 128).notNullable();
+      table.text('otherOptions'); // is NULL for open questions
+      // foreign key info to 'quizzes' table
+      table
+        .integer('quizId_fk')
+        .unsigned()
+        .notNullable()
+        .references('quizId')
+        .inTable('quizzes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
     })
     .createTable('user', table => {
-        table.increments('userId');
-        table.text('username', 128).notNullable();
-        table.text('password', 128).notNullable();
-        table.timestamps(true,true);
-    })
+      table.increments('userId');
+      table.text('username', 128).notNullable();
+      table.text('password', 128).notNullable();
+      table.timestamps(true, true);
+    });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('questions').dropTableIfExists('quizzes');
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('questions')
+    .dropTableIfExists('quizzes');
 };
