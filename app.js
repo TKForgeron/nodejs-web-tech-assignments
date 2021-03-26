a89f085808eefd96cc7616902c0e8f7aab767f18
DouweGeurtjens <d.geurtjens@students.uu.nl>
2021-03-26 12:17:55 +0100

    merging this went wrong so now i fixed it

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 #!/usr/bin nodejs
-const http = require('http');
+var http = require('http');
+var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var path = require('path');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World.');
-});
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Session
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ path: '/', httpOnly: true, secure: false, maxAge: null, secret: "session", resave: false, saveUninitialized: false }));
+
+
+var registerRouter = require('./routes/register');
+var profileRouter = require('./routes/profile');
+var loginRouter = require('./routes/login');
+
+app.use('/register', registerRouter);
+app.use('/profile', profileRouter);
+app.use('/login', loginRouter);
+
+var server = app.listen(port, function() { console.log(`Server Listening on port ${port}`); });
 
 server.on('connection', (socket) => {
-    console.log(`New Connection on port ${port}...`);
+    console.log("New Connection...");
 });
-
-const port = process.env.PORT || 3000;
-server.listen(port, 'localhost');
