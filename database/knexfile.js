b0ac5e65df87c5e60e33ca61f389bb0588f99b51
DouweGeurtjens <d.geurtjens@students.uu.nl>
2021-03-27 16:01:07 +0100

    knexfile with proper paths, seems like dbHelper uses non-existing tables

--- database/knexfile.js
+++ database/knexfile.js
@@ -3,9 +3,8 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './data/database.db3' },
-    migrations: { directory: __dirname + '/data/migrations' },
-    seeds: { directory: './data/seeds/' },
+    connection: { filename: `${__dirname}/data/database.db3` },
+    migrations: { directory: `${__dirname}/data/migrations` },    
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
