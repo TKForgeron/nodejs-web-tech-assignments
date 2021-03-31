01e58516787f88dbe0b99dd4127887849af4b302
TKForgeron <trollololollolol.js@gmail.com>
2021-03-31 22:53:35 +0200

    tested frontend connection to db

--- controllers/dbUpdaters/updateProgressController.js
+++ controllers/dbUpdaters/updateProgressController.js
@@ -7,9 +7,11 @@ module.exports = (req, res) => {
 
   userStatsArray.forEach((stat, index) => {
     const existingUserStats = dbFinder.findStatsByUserId(stat.userId_fk);
+
     // if obj is not empty then userStatsArray[index] exists in the database and then we can freely update, if not we have to add it
     let obj = existingUserStats.find(o => o.quizId_fk == stat.quizId_fk);
-    if(!obj){
+
+    if (!obj) {
       //just add
       // we couldn't find the current userStatsArray[index] object so we have to add that
       dbAdder.addUserStat(
@@ -18,19 +20,18 @@ module.exports = (req, res) => {
         userStatsArray[index].quizProgress,
         userStatsArray[index].quizSuccessRate
       );
-    }
-    else{
+    } else {
       //just update
       dbUpdater
-          .updateUserStat(obj.id, changes)
-          .then(res => {
-            console.log(
-              `userStat: ${obj.id} successfully updated, result: ${res}`
-            );
-          })
-          .catch(err => {
-            console.log(`userStat: ${iobj.id} not updated, ${err}`);
-          });
-    }      
+        .updateUserStat(obj.id, changes)
+        .then(res => {
+          console.log(
+            `userStat: ${obj.id} successfully updated, result: ${res}`
+          );
+        })
+        .catch(err => {
+          console.log(`userStat: ${iobj.id} not updated, ${err}`);
+        });
+    }
   });
 };
