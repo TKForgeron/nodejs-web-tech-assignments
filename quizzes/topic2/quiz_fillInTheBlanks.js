b4a30d853d440d6f9b5e79d2d944395475a9b255
Maarten Henzen <m.henzen@students.uu.nl>
2021-04-08 14:11:43 +0200

    Last quiz done

---
+++ quizzes/topic2/quiz_fillInTheBlanks.js
@@ -0,0 +1,38 @@
+// Open: id, title, image, question, explanation, answer, reference
+// Multiple choice: id, title, image, question, explanation, answer, otherOptions, reference
+
+// Topic2: Coding related
+
+// Multiple choice
+var question1 = {
+    id: 0,
+    title: "Fill in the blanks",
+    image: "/images/t2_fillInTheBlanks/q1.PNG",
+    question: "Please fill in the word that belongs on the underscores in the image",
+    explanation: "",
+    answer: "this",
+    otherOptions: ["these", "that", "Nothing"],
+    reference: "/examples.html" // First example
+}
+
+// Open
+var question2 = {
+    id: 1,
+    title: "Fill in the blank",
+    image: "/images/t2_fillInTheBlanks/q2.PNG",
+    question: "Please fill in the word that belongs on the underscores in the image",
+    explanation: "",
+    answer: "\"demo\"",
+    reference: "/examples.html" // Second example
+}
+
+// Open
+var question3 = {
+    id: 2,
+    title: "Fill in the blank",
+    image: "/images/t2_fillInTheBlanks/q3.PNG",
+    question: "Please fill in the word that belongs on the underscores in the image",
+    explanation: "",
+    answer: "constructor",
+    reference: "/examples.html" // Last example
+}
