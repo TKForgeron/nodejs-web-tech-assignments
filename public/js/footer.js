a03ba536fed2a4277f60395d2b59739fc434b11f
Maarten Henzen <m.henzen@students.uu.nl>
2021-04-08 14:19:11 +0200

    Creation of navbar and footer put in separate files to enhance readability

---
+++ public/js/footer.js
@@ -0,0 +1,20 @@
+// This just creates the footer
+function createFooter() {
+    var footer = document.getElementsByTagName('footer')[0];
+
+    var footerP = document.createElement('p');
+    footerP.appendChild(
+        document.createTextNode('Made by Douwe, Tim and Maarten.')
+    );
+
+    var footerImg = document.createElement('img');
+    footerImg.classList.add('footer__image');
+    footerImg.setAttribute('src', 'images/uulogotext.png');
+    footerImg.setAttribute(
+        'alt',
+        'UU Logo with Text reading Universiteit Utrecht'
+    );
+
+    footer.appendChild(footerP);
+    footer.appendChild(footerImg);
+}
---
+++ public/js/nav.js
@@ -0,0 +1,71 @@
+// This just creates the navbar.
+function createNav() {
+    var nav = document.getElementsByTagName('nav')[0];
+    var navItemClass = 'nav__item';
+    var navItemAClass = 'nav__link';
+
+    var unorderedNavList = document.createElement('ul');
+    unorderedNavList.classList.add('nav__bar');
+    unorderedNavList.classList.add('list--noBullets');
+
+    var indexItem = document.createElement('li');
+    indexItem.classList.add(navItemClass);
+    var indexAHref = document.createElement('a');
+    indexAHref.classList.add(navItemAClass);
+    indexAHref.setAttribute('href', 'index.html');
+    indexAHref.appendChild(document.createTextNode('Home'));
+    indexItem.appendChild(indexAHref);
+
+    var historyItem = document.createElement('li');
+    historyItem.classList.add(navItemClass);
+    var historyAHref = document.createElement('a');
+    historyAHref.classList.add(navItemAClass);
+    historyAHref.setAttribute('href', 'history.html');
+    historyAHref.appendChild(document.createTextNode('History'));
+    historyItem.appendChild(historyAHref);
+
+    var versionsItem = document.createElement('li');
+    versionsItem.classList.add(navItemClass);
+    var versionsAHref = document.createElement('a');
+    versionsAHref.classList.add(navItemAClass);
+    versionsAHref.setAttribute('href', 'versionsFrameworks.html');
+    versionsAHref.appendChild(document.createTextNode('Versions & Frameworks'));
+    versionsItem.appendChild(versionsAHref);
+
+    var examplesItem = document.createElement('li');
+    examplesItem.classList.add(navItemClass);
+    var examplesAHref = document.createElement('a');
+    examplesAHref.classList.add(navItemAClass);
+    examplesAHref.setAttribute('href', 'examples.html');
+    examplesAHref.appendChild(document.createTextNode('Examples'));
+    examplesItem.appendChild(examplesAHref);
+
+    var guidesItem = document.createElement('li');
+    guidesItem.classList.add(navItemClass);
+    var guidesAHref = document.createElement('a');
+    guidesAHref.classList.add(navItemAClass);
+    guidesAHref.setAttribute('href', 'guides.html');
+    guidesAHref.appendChild(document.createTextNode('Helpful guides'));
+    guidesItem.appendChild(guidesAHref);
+
+    var spacerItem = document.createElement('li');
+    spacerItem.classList.add(navItemClass + '--spacer');
+
+    var assessmentItem = document.createElement('li');
+    assessmentItem.classList.add(navItemClass);
+    assessmentItem.classList.add(navItemClass + '--right');
+    var assessmentAHref = document.createElement('a');
+    assessmentAHref.classList.add(navItemAClass + '--active');
+    assessmentAHref.setAttribute('href', 'assessment.html');
+    assessmentAHref.appendChild(document.createTextNode('Assessment'));
+    assessmentItem.appendChild(assessmentAHref);
+
+    unorderedNavList.appendChild(indexItem);
+    unorderedNavList.appendChild(historyItem);
+    unorderedNavList.appendChild(versionsItem);
+    unorderedNavList.appendChild(examplesItem);
+    unorderedNavList.appendChild(guidesItem);
+    unorderedNavList.appendChild(spacerItem);
+    unorderedNavList.appendChild(assessmentItem);
+    nav.appendChild(unorderedNavList);
+}
