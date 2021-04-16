18917681b72a3c6dfcba2a7751b6b6127f8f5bfb
DouweGeurtjens <d.geurtjens@students.uu.nl>
2021-04-16 16:33:48 +0200

    fixed quiz for not logged in people

--- controllers/sessionProgressGetter.js
+++ controllers/sessionProgressGetter.js
@@ -1,3 +1,17 @@
 module.exports = (req, res) => {  
-    res.status(200).send(req.session.progressArray);
+    if(req.session.loggedin){
+        res.status(200).send(req.session.progressArray);
+    }
+    else{
+        const arr = [
+            [[], []],
+            [[], []],
+          ];
+        
+          arr[0][0] = 0;
+          arr[0][1] = 0;
+          arr[1][0] = 0;
+          arr[1][1] = 0;
+        res.status(200).send(arr);
+    }
 };
