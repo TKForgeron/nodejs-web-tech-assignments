bacc050a407c37be91b160ad1c2c0157fb618cfb
Maarten Henzen <m.henzen@students.uu.nl>
2021-04-17 12:49:08 +0200

    Second quiz added

--- quizzes/topic1/quiz_versionsAndFrameworks.js
+++ quizzes/topic1/quiz_versionsAndFrameworks.js
@@ -7,21 +7,21 @@
 var question1 = {
     id: 0,
     title: "Versions",
-    image: "", // Not needed
-    question: "Since what version/when did they stop bringing out versions and start bringing out yearly updates? ",
+    image: "/images/t1_versionsAndFrameworks/q1.jpeg", // Not needed
+    question: "Since what version did they stop bringing out versions and start bringing out yearly updates? ",
     explanation: "/versionsFrameworks.html", // Table/text above table, 2nd line
-    answer: "ES6/2016"
+    answer: "ES6"
 }
 
 // Multiple choice
 var question2 = {
     id: 1,
     title: "Usage",
-    image: "", // Not needed
+    image: "/images/t1_versionsAndFrameworks/q2.png", // Not needed
     question: "Where is JavaScript good for?",
     explanation: "/versionsFrameworks.html", // Frameworks section, 2nd line
     answer: "To create and control dynamic web components",
-    otherOptions: ["To create and control static web components","To learn programming Java in an easier way","JavaScript isn't good for anything"]
+    otherOptions: ["To create and control static web components","To learn programming Java in an easier way","JavaScript is not good for anything"]
 }
 
 // Multiple choice
@@ -32,5 +32,5 @@ var question3 = {
     question: "What is the idea behind a JavaScript framework?",
     explanation: "/versionsFrameworks.html", // What is a JavaScript framework? section, quote
     answer: "JavaScript frameworks provide developers with pre-written JavaScript code to use for routine programming features and tasks",
-    otherOptions: ["To create more robust JavaScript code","It is just JavaScript, but with a different syntax than used in vanilla JavaScript, just like an accent of a language","None of the above"]
+    otherOptions: ["To create more robust JavaScript code","It is just JavaScript but with a different syntax than used in vanilla JavaScript just like an accent of a language","None of the above"]
 }
